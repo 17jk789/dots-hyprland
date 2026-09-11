84e672a13cad74d4f82e8565ab017f0ad59a8834
JK <julian.kropff@icloud.com>
2026-09-11 14:44:42 +0200

    fix(jk-arch-config): glass toggle: add more apps to glass exclusion list

--- dots/.config/fish/functions/glasstoggle.fish
+++ dots/.config/fish/functions/glasstoggle.fish
@@ -384,7 +384,7 @@ function glasstoggle --description "Toggle Hyprland glass mode and Illogical Imp
             '-- Personal applications excluded from glass' \
             'hl.window_rule({' \
             '    match = {' \
-            '        class = "^(brave-browser|Blender|resolve|com.blackmagicdesign.resolve|kitty|Alacritty|ghostty|firefox|firefox-developer-edition|libreoffice|libreoffice-startcenter|org.wireshark.Wireshark|wireshark|org.kde.gwenview|org.kde.okular|com.obsproject.Studio)$",' \
+            '        class = "^(brave-browser|blender|resolve|com.blackmagicdesign.resolve|kitty|Alacritty|com.mitchellh.ghostty|firefox|firefox-developer-edition|org.kde.gwenview|org.kde.okular|com.obsproject.Studio|org.kde.krita|gimp|org.pwmt.zathura|org.gnome.Evince)$",' \
             '    },' \
             '    opacity = 1.0,' \
             '})' \
@@ -415,7 +415,7 @@ function glasstoggle --description "Toggle Hyprland glass mode and Illogical Imp
             '-- Applications excluded from glass' \
             'hl.window_rule({' \
             '    match = {' \
-            '        class = "^(code|Code|com.jetbrains.*|jetbrains-.*|brave-browser|Blender|resolve|com.blackmagicdesign.resolve|kitty|Alacritty|ghostty|firefox|firefox-developer-edition|libreoffice|libreoffice-startcenter|org.wireshark.Wireshark|wireshark|org.kde.gwenview|org.kde.okular|com.obsproject.Studio)$",' \
+            '        class = "^(code|Code|com.jetbrains.*|jetbrains-.*|libreoffice|libreoffice-writer|libreoffice-calc|libreoffice-impress|libreoffice-draw|libreoffice-base|libreoffice-math|libreoffice-startcenter|org.wireshark.Wireshark|wireshark|brave-browser|blender|resolve|com.blackmagicdesign.resolve|kitty|Alacritty|com.mitchellh.ghostty|firefox|firefox-developer-edition|org.kde.gwenview|org.kde.okular|com.obsproject.Studio|org.kde.krita|gimp|org.pwmt.zathura|org.gnome.Evince)$",' \
             '    },' \
             '    opacity = 1.0,' \
             '})' \
